88c2fbd546ffa92ee5e3b66a73b5da313c453c3f
Keeyong (Max) <keeyonghan@hotmail.com>
2022-04-30 20:56:59 -0700

    Create tutorial.py

---
+++ pyspark/tutorial.py
@@ -0,0 +1,15 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+
+S3_DATA_INPUT_PATH = 's3://spark-tutorial-dataset/survey_results_public.csv'
+S3_DATA_OUTPUT_PATH = 's3://spark-tutorial-dataset/data-output'
+
+spark = SparkSession.builder.appName('Tutorial').getOrCreate()
+
+df = spark.read.csv(S3_DATA_INPUT_PATH, header=True)
+print('# of records {}'.format(df.count()))
+
+learnCodeUS = df.where((col('Country') == 'United States')).groupby('LearnCode').count()
+
+learnCodeUS.write.mode('overwrite').csv(S3_DATA_OUTPUT_PATH)  # parquet
+print('Selected data is successfully saved to S3: {}'.format(S3_DATA_OUTPUT_PATH))
