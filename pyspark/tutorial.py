c11877c88fbc5153629a20cbdd63a989776ea045
Keeyong (Max) <keeyonghan@hotmail.com>
2022-05-01 00:29:02 -0700

    Update tutorial.py

--- pyspark/tutorial.py
+++ pyspark/tutorial.py
@@ -9,7 +9,7 @@ spark = SparkSession.builder.appName('Tutorial').getOrCreate()
 df = spark.read.csv(S3_DATA_INPUT_PATH, header=True)
 print('# of records {}'.format(df.count()))
 
-learnCodeUS = df.where((col('Country') == 'United States')).groupby('LearnCode').count()
+learnCodeUS = df.where((col('Country') == 'United States of America')).groupby('LearnCode').count()
 
 learnCodeUS.write.mode('overwrite').csv(S3_DATA_OUTPUT_PATH)  # parquet
 print('Selected data is successfully saved to S3: {}'.format(S3_DATA_OUTPUT_PATH))
